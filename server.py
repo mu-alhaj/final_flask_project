88b24e454404efe2669bfbd079ba59a974ee52bc
Your Name <you@example.com>
2025-08-22 09:37:08 -0400

    added server

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/")
+def render_index():
+    return render_template("index.html")
+
+@app.route("/emotionDetector")
+def emotion_detector_fun():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    output_text = f"For the given statement, the system response is 'anger': {response['anger']}, \
+    'disgust': {response['disgust']}, \
+    'fear': {response['fear']}, \
+    'joy': {response['joy']} \
+    and 'sadness': {response['sadness']}. \
+    The dominant emotion is {response['dominant_emotion']}."
+
+    return output_text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
